96b0b0f9ab5acc912c164d7e758e3303d0c70271
opuopu <nurmdopu428@gmail.com>
2024-06-30 17:58:53 +0600

    feat:change logo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "143.198.197.69",
+    port: 5173,
+  },
 });
