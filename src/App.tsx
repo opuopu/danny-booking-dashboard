a6b6bcf547a3cf0ad02ed1593718d2efeb718f9f
opuopu <nurmdopu428@gmail.com>
2025-05-12 10:28:59 +0600

    fix:fixed socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect } from "react";
 import "./App.css";
 import MainLayout from "./layout/MainLayout";
 import { TUser, useCurrentUser } from "./redux/features/auth/authSlice";
-import { setNotification } from "./redux/features/notification/notificationSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 // import PrivateRoute from "./router/PrivateRoutes";
 import PrivateRoute from "./router/PrivateRoutes";
-import { socket } from "./socket";
 
 function App() {
   const dispatch = useAppDispatch();
   const user: TUser | null = useAppSelector(useCurrentUser);
-  useEffect(() => {
-    socket.connect();
-    const handleNotificationEvent = (data: any) => {
-      dispatch(setNotification(data));
-    };
+  // useEffect(() => {
+  //   socket.connect();
+  //   const handleNotificationEvent = (data: any) => {
+  //     dispatch(setNotification(data));
+  //   };
 
-    socket.on("notification", handleNotificationEvent);
+  //   socket.on("notification", handleNotificationEvent);
 
-    return () => {
-      // Clean up the event listener when the component is unmounted
-      socket.off(user?.userId as string, handleNotificationEvent);
-      socket.disconnect();
-    };
-  }, [user]);
+  //   return () => {
+  //     // Clean up the event listener when the component is unmounted
+  //     socket.off(user?.userId as string, handleNotificationEvent);
+  //     socket.disconnect();
+  //   };
+  // }, [user]);
 
   return (
     <PrivateRoute role={undefined}>
